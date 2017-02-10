7b26c7d12aaa17abf8ffcbd46d3761940ed02c35
Carlos Campos <danger572.cc@gmail.com>
2017-02-10 00:09:54 -0400

    Pequeñas correciones.

--- Ppgz/Ppgz.Web/Models/AccountViewModels.cs
+++ Ppgz/Ppgz.Web/Models/AccountViewModels.cs
@@ -2,90 +2,90 @@
 
 namespace Ppgz.Web.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
-    }
+	public class ExternalLoginConfirmationViewModel
+	{
+		[Required]
+		[Display(Name = @"User name")]
+		public string UserName { get; set; }
+	}
 
-    public class ManageUserViewModel
-    {
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
+	public class ManageUserViewModel
+	{
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = @"Current password")]
+		public string OldPassword { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New password")]
-        public string NewPassword { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = @"The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[Display(Name = @"New password")]
+		public string NewPassword { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
+		[DataType(DataType.Password)]
+		[Display(Name = @"Confirm new password")]
+		[Compare(@"NewPassword", ErrorMessage = @"The new password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
+	}
 
-    public class LoginViewModel
-    {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
+	public class LoginViewModel
+	{
+		[Required]
+		[Display(Name = @"User name")]
+		public string UserName { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = @"Password")]
+		public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
-    }
+		[Display(Name = @"Remember me?")]
+		public bool RememberMe { get; set; }
+	}
 
-    public class RegisterViewModel
-    {
-        // Company Info
+	public class RegisterViewModel
+	{
+		// Company Info
 
-        [Required]
-        [Display(Name = "RFC")]
-        public string EmpresaRfc { get; set; }
+		[Required]
+		[Display(Name = @"RFC")]
+		public string EmpresaRfc { get; set; }
 
-        [Required]
-        [Display(Name = "Razón social")]
-        public string EmpresaRazonSocial { get; set; }
+		[Required]
+		[Display(Name = @"Razón social")]
+		public string EmpresaRazonSocial { get; set; }
 
-        // Responsible Info
+		// Responsible Info
 
-        [Required]
-        [Display(Name = "Nombre")]
-        public string ResponsableNombre { get; set; }
-        
-        [Required]
-        [Display(Name = "Apellido")]
-        public string ResponsableApellido { get; set; }
+		[Required]
+		[Display(Name = @"Nombre")]
+		public string ResponsableNombre { get; set; }
 
-        [Required]
-        [Display(Name = "Correo electrónico")]
-        public string ResponsableEmail { get; set; }
+		[Required]
+		[Display(Name = @"Apellido")]
+		public string ResponsableApellido { get; set; }
 
-        [Required]
-        [Display(Name = "Teléfono")]
-        public string ResponsableTelefono { get; set; }
+		[Required]
+		[Display(Name = @"Correo electrónico")]
+		public string ResponsableEmail { get; set; }
 
-        [Required]
-        [Display(Name = "Nombre de Usuario")]
-        public string NombreUsuario { get; set; }
+		[Required]
+		[Display(Name = @"Teléfono")]
+		public string ResponsableTelefono { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Contraseña")]
-        public string Password { get; set; }
+		[Required]
+		[Display(Name = @"Nombre de Usuario")]
+		public string NombreUsuario { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirmar contraseña")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
+		[Required]
+		[StringLength(100, ErrorMessage = @"The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[Display(Name = @"Contraseña")]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Display(Name = @"Confirmar contraseña")]
+		[Compare(@"Password", ErrorMessage = @"The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
+	}
 }
--- Ppgz/Ppgz.Web/Models/IdentityModels.cs
+++ Ppgz/Ppgz.Web/Models/IdentityModels.cs
@@ -2,29 +2,27 @@
 
 namespace Ppgz.Web.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public string EmpresaRfc { get; set; }
+	// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+	public class ApplicationUser : IdentityUser
+	{
+		public string EmpresaRfc { get; set; }
 
-        public string EmpresaRazonSocial { get; set; }
+		public string EmpresaRazonSocial { get; set; }
 
-        public string ResponsableNombre { get; set; }
-        
-        public string ResponsableApellido { get; set; }
-        
-        public string ResponsableEmail { get; set; }
+		public string ResponsableNombre { get; set; }
 
-        public string ResponsableTelefono { get; set; }
+		public string ResponsableApellido { get; set; }
 
+		public string ResponsableEmail { get; set; }
 
-    }
+		public string ResponsableTelefono { get; set; }
+	}
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection")
-        {
-        }
-    }
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext()
+			: base(@"DefaultConnection")
+		{
+		}
+	}
 }
