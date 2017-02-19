e5e24ebad9b89e20a4af3751bff2009dc6daf3d1
Juan Godoy <g.juanch14@gmail.com>
2017-02-19 18:00:13 -0400

    Implementación de CustomAttribute para Auth

---
+++ Ppgz/Ppgz.Web/Infraestructure/ExtendedMethods_Principal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Ppgz.Web.Infraestructure
+{
+    public static class ExtendedMethods_Principal
+    {
+        public static bool HasPermission(this IPrincipal _principal, string _requiredPermission)
+        {
+            bool _retVal = false;
+           /* try
+            {
+                if (_principal != null && _principal.Identity != null && _principal.Identity.IsAuthenticated)
+                {
+                    var ci = _principal.Identity as ClaimsIdentity;
+                    string _userId = ci != null ? ci.FindFirstValue(ClaimTypes.NameIdentifier) : null;
+
+                    if (!string.IsNullOrEmpty(_userId))
+                    {
+                        ApplicationUser _authenticatedUser = ApplicationUserManager.GetUser(int.Parse(_userId));
+                        _retVal = _authenticatedUser.IsPermissionInUserRoles(_requiredPermission);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }*/
+            return _retVal;
+        }
+
+        public static bool IsSysAdmin(this IPrincipal _principal)
+        {
+            bool _retVal = false;
+            /*try
+            {
+                if (_principal != null && _principal.Identity != null && _principal.Identity.IsAuthenticated)
+                {
+                    var ci = _principal.Identity as ClaimsIdentity;
+                    string _userId = ci != null ? ci.FindFirstValue(ClaimTypes.NameIdentifier) : null;
+
+                    if (!string.IsNullOrEmpty(_userId))
+                    {
+                        ApplicationUser _authenticatedUser = ApplicationUserManager.GetUser(int.Parse(_userId));
+                        _retVal = _authenticatedUser.IsSysAdmin();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }*/
+            return _retVal;
+        }
+
+        public static string FindFirstValue(this ClaimsIdentity identity, string claimType)
+        {
+            string _retVal = string.Empty;
+            try
+            {
+                if (identity != null)
+                {
+                    var claim = identity.FindFirst(claimType);
+                    _retVal = claim != null ? claim.Value : null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return _retVal;
+        }
+    }
+}
