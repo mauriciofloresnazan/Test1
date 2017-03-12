85ef7cfc196f11547763bd912479e337f5a80ca2
Jean Peraza <jeanperaza75@gmail.com>
2017-03-12 15:56:47 -0400

    CONTROLADOR Y VISTA DE CITAS

---
+++ Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ControlCitasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ppgz.Web.Areas.Mercaderia.Controllers
+{
+    public class ControlCitasController : Controller
+    {
+        //
+        // GET: /Mercaderia/ControlCitas/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
