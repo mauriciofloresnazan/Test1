4ccfb689e2918844fd98c9ad839f49bc28993639
Juan Godoy <g.juanch14@gmail.com>
2017-03-12 20:41:35 -0400

    Manejador de Ordenes de Compra

---
+++ Ppgz/Ppgz.Web/Infrastructure/OrdenCompraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ppgz.Repository;
+
+namespace Ppgz.Web.Infrastructure
+{
+    public class OrdenCompraManager
+    {
+        private readonly Entities _db = new Entities();
+        public List<ordencompra> FindByProveedorId(int id )
+        {
+            return _db.ordencompras.Where(o => o.ProveedoresId == id).ToList();
+
+        } 
+    }
+}
