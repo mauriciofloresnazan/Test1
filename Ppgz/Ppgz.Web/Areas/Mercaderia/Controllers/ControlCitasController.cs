97dda4c807b7b188361f68606ca8b0be67070e35
Jean Peraza <jeanperaza75@gmail.com>
2017-03-14 19:07:06 -0400

    FUNCIONALIDAD INICIAL DEL MODULO DE CITAS Se agrego la funcionalidad de busqueda del proveedor y la seleccion de la orden de compra

--- Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ControlCitasController.cs
+++ Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ControlCitasController.cs
@@ -1,18 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using Ppgz.Repository;
+using Ppgz.Web.Infrastructure;
 
 namespace Ppgz.Web.Areas.Mercaderia.Controllers
 {
     public class ControlCitasController : Controller
     {
+
+
+        readonly ProveedorManager _proveedorManager = new ProveedorManager();
+        readonly CommonManager _commonManager = new CommonManager();
+        readonly CuentasPorPagarManager _cuentasPorPagarManager = new CuentasPorPagarManager();
+
         //
         // GET: /Mercaderia/ControlCitas/
         public ActionResult Index()
         {
+
+            var cuenta = _commonManager.GetCuentaUsuarioAutenticado();
+
+            ViewBag.proveedores = _proveedorManager.FindByCuentaId(cuenta.Id);
+            
             return View();
         }
+
+
+        public ActionResult OrdenDeCompra(int proveedorId)
+        {
+            
+            return View();
+        }
+
+
 	}
 }
