3d09960f56b7bd20b2f08404615aa9a8cf43584a
Juan Godoy <g.juanch14@gmail.com>
2017-03-24 09:04:28 -0400

    Inclusión del Campo RFC en el Wrapper de Sap Gestión de Proveedores

--- Ppgz/Ppgz.Services/ProveedorManager.cs
+++ Ppgz/Ppgz.Services/ProveedorManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Ppgz.Repository;
 using SapWrapper;
@@ -35,47 +34,43 @@ namespace Ppgz.Services
                 throw new BusinessException(CommonMensajesResource.ERRO_Sap_ProveedorCodigo);
             }
 
-            try
+ 
+            var proveedor = new proveedore
             {
-                var proveedor = new proveedore
-                {
-                    NumeroProveedor = result["LIFNR"].ToString(),
-                    ClavePais = result["LAND1"].ToString(),
-                    Nombre1 = result["NAME1"].ToString(),
-                    Nombre2 = result["NAME2"].ToString(),
-                    Nombre3 = result["NAME3"].ToString(),
-                    Nombre4 = result["NAME4"].ToString(),
-                    Poblacion = result["ORT01"].ToString(),
-                    Distrito = result["ORT02"].ToString(),
-                    Apartado = result["PFACH"].ToString(),
-                    CodigoApartado = result["PSTL2"].ToString(),
-                    CodigoPostal = result["PSTLZ"].ToString(),
-                    Region = result["REGIO"].ToString(),
-                    Calle = result["STRAS"].ToString(),
-                    Direccion = result["ADRNR"].ToString(),
-                    Sociedad = result["BUKRS"].ToString(),
-                    OrganizacionCompra = result["EKORG"].ToString(),
-                    ClaveMoned = result["WAERS"].ToString(),
-                    VendedorResponsable = result["VERKF"].ToString(),
-                    NumeroTelefono = result["TELF1"].ToString(),
-                    CondicionPago = result["ZTERM"].ToString(),
-                    IncoTerminos1 = result["INCO1"].ToString(),
-                    IncoTerminos2 = result["INCO2"].ToString(),
-                    GrupoCompras = result["EKGRP"].ToString(),
-                    DenominacionGrupo = result["EKNAM"].ToString(),
-                    TelefonoGrupoCompra = result["EKTEL"].ToString(),
-                    TelefonoPrefijo = result["TEL_NUMER"].ToString(),
-                    TelefonoExtension = result["TEL_EXTENS"].ToString(),
-                    Correo = result["SMTP_ADDR"].ToString()
-                };
+                NumeroProveedor = result["LIFNR"].ToString(),
+                ClavePais = result["LAND1"].ToString(),
+                Nombre1 = result["NAME1"].ToString(),
+                Nombre2 = result["NAME2"].ToString(),
+                Nombre3 = result["NAME3"].ToString(),
+                Nombre4 = result["NAME4"].ToString(),
+                Poblacion = result["ORT01"].ToString(),
+                Distrito = result["ORT02"].ToString(),
+                Apartado = result["PFACH"].ToString(),
+                CodigoApartado = result["PSTL2"].ToString(),
+                CodigoPostal = result["PSTLZ"].ToString(),
+                Region = result["REGIO"].ToString(),
+                Calle = result["STRAS"].ToString(),
+                Direccion = result["ADRNR"].ToString(),
+                Sociedad = result["BUKRS"].ToString(),
+                OrganizacionCompra = result["EKORG"].ToString(),
+                ClaveMoned = result["WAERS"].ToString(),
+                VendedorResponsable = result["VERKF"].ToString(),
+                NumeroTelefono = result["TELF1"].ToString(),
+                CondicionPago = result["ZTERM"].ToString(),
+                IncoTerminos1 = result["INCO1"].ToString(),
+                IncoTerminos2 = result["INCO2"].ToString(),
+                GrupoCompras = result["EKGRP"].ToString(),
+                DenominacionGrupo = result["EKNAM"].ToString(),
+                TelefonoGrupoCompra = result["EKTEL"].ToString(),
+                TelefonoPrefijo = result["TEL_NUMER"].ToString(),
+                TelefonoExtension = result["TEL_EXTENS"].ToString(),
+                Correo = result["SMTP_ADDR"].ToString(),
+                Rfc = result["STCD1"].ToString()
+            };
 
-                return proveedor;
-            }
-            catch (NullReferenceException nullReferenceException)
-            {
-                // TODO
-                throw;
-            }
+            return proveedor;
+      
+
 
 
         }
