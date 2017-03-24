75eba80c652acecd7557b8a66f30f04f339382b4
Juan Godoy <g.juanch14@gmail.com>
2017-03-24 09:53:08 -0400

    Ajuste de la consulta del RFC de proveedores

--- Ppgz/SapWrapper/SapProveedorManager.cs
+++ Ppgz/SapWrapper/SapProveedorManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Reflection.Emit;
 using SAP.Middleware.Connector;
 
 namespace SapWrapper
@@ -8,28 +9,26 @@ namespace SapWrapper
     {
         private readonly CommonRfcConfigParam _rfc = new CommonRfcConfigParam();
 
-        public DataRow GetProveedor(string codigoProveedor)
+        public DataRow GetProveedor(string numeroProveedor)
         {
+            if (String.IsNullOrWhiteSpace(numeroProveedor.Trim()))
+            {
+                //TODO
+                throw new Exception("Número de proveedor incorrecto");
+            }
+
             var rfcDestinationManager = RfcDestinationManager.GetDestination(_rfc);
             var rfcRepository = rfcDestinationManager.Repository;
             var function = rfcRepository.CreateFunction("ZEXTRAE_PROV");
-            function.SetValue("LIFNR", codigoProveedor);
+            function.SetValue("LIFNR", numeroProveedor);
             function.Invoke(rfcDestinationManager);
 
             var result = function.GetTable("T_PROV");
 
-            if (result.Count == 1)
-            {
-                var dt = result.ToDataTable("T_PROV");
-                return dt.Rows[0];
-            }
-            if (result.Count > 1)
-            {
-                //TODO 
-                throw new Exception("Multiples Registros con el mismo codigo de proveedor en SAP");
-            }
+            if (result.Count <= 0) return null;
 
-            return null;
+            var dt = result.ToDataTable("T_PROV");
+            return dt.Rows[0];
         }
 
     }
