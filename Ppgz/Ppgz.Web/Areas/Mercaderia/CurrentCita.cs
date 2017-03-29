cf511c8b185d5711d70273d0d78853f16a884940
Juan Godoy <g.juanch14@gmail.com>
2017-03-28 21:11:34 -0400

    Actualizacion de Clase para la cita en proceso

--- Ppgz/Ppgz.Web/Areas/Mercaderia/CurrentCita.cs
+++ Ppgz/Ppgz.Web/Areas/Mercaderia/CurrentCita.cs
@@ -55,5 +55,72 @@ namespace Ppgz.Web.Areas.Mercaderia
         }
 
 
+        public int UpdateElementoEnLista(string numeroDocumento, string numeroMaterial, 
+            int oldValue, int newValue)
+        {
+
+            /**************************************
+            METODO PARA ACTUALIZAR LA CANTIDAD
+             DE LA TALLA, EXISTENTE EN UN ITEMS
+             PARA UN NUMERO DE ORDEN
+            ***************************************/
+
+            var resultado = 0;
+
+            var orden = Ordenes.FirstOrDefault(o => o.NumeroDocumento == numeroDocumento);
+
+            if (orden == null)
+            {
+                return resultado;
+            }
+
+            if (!orden.ordencompradetalles.Any())
+            {
+                return resultado;
+            }
+
+            var detalle = orden.ordencompradetalles.FirstOrDefault(d => d.NumeroMaterial == numeroMaterial);
+          
+            if (detalle == null)
+            {
+                return resultado;
+            }
+
+            detalle.CantidadComprometida = newValue;
+            resultado = 1;
+
+
+            /**************************************
+            AQUI SE RECORRE LA LISTA CON LAS ORDENES
+            ***************************************/
+            /*foreach (var ordenes in _list)
+            {
+                if (ordenes.Id == Orden)//ORDEN A BUSCAR
+                {
+
+                    //List<ordencompradetalle> ItemsEnOrden = new List<ordencompradetalle>();
+                    List<ordencompradetalle> resultado = new List<ordencompradetalle>((List<ordencompradetalle>)ordenes.Valores);
+                    //ItemsOrdenes.AddRange(resultado);
+
+                    foreach (var item in resultado)
+                    {
+                        if (item.NumeroMaterial == Item)
+                        {
+                            item.CantidadComprometida = Convert.ToDecimal(NewValue);
+                            Resultado = 1;
+                            break;
+                        }
+
+                    }
+
+
+                }
+            }
+            */
+            return resultado;
+
+        }
+
+
     }
 }
