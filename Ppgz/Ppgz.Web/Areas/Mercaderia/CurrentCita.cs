9e6076d3840cf2cc755b4f9aeba099fef5ff77f8
Juan Godoy <g.juanch14@gmail.com>
2017-03-28 20:48:20 -0400

    Clase para la cita en proceso

---
+++ Ppgz/Ppgz.Web/Areas/Mercaderia/CurrentCita.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ppgz.Repository;
+using Ppgz.Services;
+
+namespace Ppgz.Web.Areas.Mercaderia
+{
+    public class CurrentCita
+    {
+        private readonly proveedore _proveedor;
+        public proveedore Proveedor
+        {
+            get
+            {
+                return _proveedor;
+                
+            }
+        }
+
+        public DateTime? Fecha;
+
+        public List<ordencompra> Ordenes;
+
+        public CurrentCita(int cuentaId, int proveedorId)
+        {
+            var proveedorManager = new ProveedorManager();
+
+            var proveedor = proveedorManager.Find(proveedorId);
+
+            if (proveedor.CuentaId != cuentaId)
+            {
+                throw new BusinessException("Error en la cuenta del proveedor");
+            }
+            _proveedor = proveedor;
+
+        }
+
+        public void AddOrden(ordencompra orden)
+        {
+            if (Ordenes.Any(o => o.Id == orden.Id))
+            {
+                throw new BusinessException("La orden ya se encuentra incluida");
+            }
+            Ordenes.Add(orden);
+        }
+
+        public void RemoveOrden(string numeroDocumento)
+        {
+            var orden = Ordenes.FirstOrDefault(o => o.NumeroDocumento == numeroDocumento);
+            if (orden != null)
+            {
+                Ordenes.Remove(orden);
+            }
+        }
+
+
+    }
+}
