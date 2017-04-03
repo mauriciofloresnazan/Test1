03e397cfcb9d01ef0784afbfd3332b19ef126089
Juan Delgado <juandelgadofreelancer@gmail.com>
2017-04-03 00:03:55 -0400

    Impresión de etiquetas

--- Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ImpresionEtiquetasController.cs
+++ Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ImpresionEtiquetasController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Mvc;
 using Ppgz.Repository;
 using Ppgz.Web.Infrastructure;
+using OrdenCompraManager = Ppgz.Services.OrdenCompraManager;
 
 namespace Ppgz.Web.Areas.Mercaderia.Controllers
 {
@@ -10,12 +11,20 @@ namespace Ppgz.Web.Areas.Mercaderia.Controllers
     [TerminosCondiciones]
     public class ImpresionEtiquetasController : Controller
     {
+        private readonly OrdenCompraManager _ordenCompraManager = new OrdenCompraManager();
+        private readonly CommonManager _commonManager = new CommonManager();
         // GET: /Mercaderia/ImpresionEtiquetas/
         [Authorize(Roles = "MAESTRO-MERCADERIA")]
         public ActionResult Index()
         {
             var db = new Entities();
+
+            var cuenta = _commonManager.GetCuentaUsuarioAutenticado();
+
+            ViewBag.data = _ordenCompraManager.FindOrdenesDecompraActivasByCuenta(cuenta.Id);
+
             ViewBag.Crs = db.crs.ToList();
+
             return View();
         }
         public FileResult Descargar(int id)
