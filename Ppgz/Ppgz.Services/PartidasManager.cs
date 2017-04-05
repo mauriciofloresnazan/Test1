d11c300600ffd20aa2daeef314ade952713d479d
Juan Godoy <g.juanch14@gmail.com>
2017-04-04 23:56:03 -0400

    Manejadores de partidas a efectos de la presentación ante el cliente

---
+++ Ppgz/Ppgz.Services/PartidasManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using SapWrapper;
+
+namespace Ppgz.Services
+{
+    public class PartidasManager
+    {
+        public DataSet GetPartidas1001(string numeroProveedor)
+        {
+            var sapPartidaManager =  new SapPartidaManager();
+
+            return sapPartidaManager.GetPartidas1001(numeroProveedor);
+        }
+    }
+}
--- Ppgz/SapWrapper/SapPartidaManager.cs
+++ Ppgz/SapWrapper/SapPartidaManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using SAP.Middleware.Connector;
 
 namespace SapWrapper
@@ -88,5 +87,73 @@ namespace SapWrapper
             return result2.ToDataTable("T_MAT_DEV");
         }
 
+        public DataSet GetPartidas(string numeroProveedor, string sociedad)
+        {
+            var rfcDestinationManager = RfcDestinationManager.GetDestination(_rfc);
+            var rfcRepository = rfcDestinationManager.Repository;
+            var function = rfcRepository.CreateFunction("ZEXTRAE_PART_PROV");
+   
+            function.SetValue("LIFNR", numeroProveedor);
+            function.SetValue("BUKRS", sociedad);
+            //TODO ELIMINAR PARA PRODUCCION
+            function.SetValue("IDIOMA", "S");
+            function.SetValue("FECHA_BASE", "20160630");
+            
+            function.Invoke(rfcDestinationManager);
+
+            var dataSet = new DataSet();
+            
+            dataSet.Tables.Add(function.GetTable("T_DEVOLUCIONES")
+                .ToDataTable("T_DEVOLUCIONES"));
+            dataSet.Tables.Add(function.GetTable("T_MAT_DEV")
+                .ToDataTable("T_MAT_DEV"));
+
+            dataSet.Tables.Add(function.GetTable("T_LISTA_PAGOS")
+                .ToDataTable("T_LISTA_PAGOS"));
+            dataSet.Tables.Add(function.GetTable("T_PAGOS")
+                .ToDataTable("T_PAGOS"));
+
+            dataSet.Tables.Add(function.GetTable("T_PARTIDAS_ABIERTAS")
+                .ToDataTable("T_PARTIDAS_ABIERTAS"));
+            dataSet.Tables.Add(function.GetTable("T_DETALLE_AB")
+                .ToDataTable("T_DETALLE_AB"));
+
+            return dataSet;
+        }
+
+        //TODO  HAY QUE PASARLE LA SOCIEDAD Y SOLO DEVUELVE DEVOLUCIONES
+        public DataSet GetPartidas1001(string numeroProveedor)
+        {
+            var rfcDestinationManager = RfcDestinationManager.GetDestination(_rfc);
+            var rfcRepository = rfcDestinationManager.Repository;
+            var function = rfcRepository.CreateFunction("ZEXTRAE_PART_PROV");
+
+            function.SetValue("LIFNR", numeroProveedor);
+            function.SetValue("BUKRS", "1001");
+            //TODO ELIMINAR PARA PRODUCCION
+            function.SetValue("IDIOMA", "S");
+            function.SetValue("FECHA_BASE", "20160630");
+
+            function.Invoke(rfcDestinationManager);
+
+            var dataSet = new DataSet();
+
+            dataSet.Tables.Add(function.GetTable("T_DEVOLUCIONES")
+                .ToDataTable("T_DEVOLUCIONES"));
+            dataSet.Tables.Add(function.GetTable("T_MAT_DEV")
+                .ToDataTable("T_MAT_DEV"));
+
+            dataSet.Tables.Add(function.GetTable("T_LISTA_PAGOS")
+                .ToDataTable("T_LISTA_PAGOS"));
+            dataSet.Tables.Add(function.GetTable("T_PAGOS")
+                .ToDataTable("T_PAGOS"));
+
+            dataSet.Tables.Add(function.GetTable("T_PARTIDAS_ABIERTAS")
+                .ToDataTable("T_PARTIDAS_ABIERTAS"));
+            dataSet.Tables.Add(function.GetTable("T_DETALLE_AB")
+                .ToDataTable("T_DETALLE_AB"));
+
+            return dataSet;
+        }
     }
 }
