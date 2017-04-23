c394787fabc5e8c2070231122ae39b1c5a33856d
Juan Godoy <g.juanch14@gmail.com>
2017-04-23 10:35:44 -0400

    Control de citas actualizacion de las reglas de negocio

--- Ppgz/Ppgz.CitaWrapper/RulesManager.cs
+++ Ppgz/Ppgz.CitaWrapper/RulesManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.NetworkInformation;
 using Ppgz.Repository;
@@ -23,9 +24,7 @@ namespace Ppgz.CitaWrapper
             return _configuraciones;
         }
         
-
-
-        internal static List<DayOfWeek> GetDiasDeSemana()
+        internal static List<DayOfWeek> GetDiasOperativos()
         {
 
             var dias = GetConfiguraciones().Single(c => c.Clave == "warehouse.working-day.enabled").Valor.Split(',');
@@ -33,27 +32,96 @@ namespace Ppgz.CitaWrapper
             var result = new List<DayOfWeek>();
             foreach (var dia in dias)
             {
-                if (dia == "0")
-                    result.Add(DayOfWeek.Sunday);
                 if (dia == "1")
-                    result.Add(DayOfWeek.Monday);
+                    result.Add(DayOfWeek.Sunday);
                 if (dia == "2")
-                    result.Add(DayOfWeek.Tuesday);
+                    result.Add(DayOfWeek.Monday);
                 if (dia == "3")
-                    result.Add(DayOfWeek.Wednesday);
+                    result.Add(DayOfWeek.Tuesday);
                 if (dia == "4")
-                    result.Add(DayOfWeek.Thursday);
+                    result.Add(DayOfWeek.Wednesday);
                 if (dia == "5")
-                    result.Add(DayOfWeek.Friday);
+                    result.Add(DayOfWeek.Thursday);
                 if (dia == "6")
+                    result.Add(DayOfWeek.Friday);
+                if (dia == "7")
                     result.Add(DayOfWeek.Saturday);
             }
             return result;
         }
+        internal static List<DayOfWeek> GetDiasEspeciales()
+        {
 
+            var dias = GetConfiguraciones().Single(c => c.Clave == "warehouse.special-day.provider").Valor.Split(',');
+
+            var result = new List<DayOfWeek>();
+            foreach (var dia in dias)
+            {
+                if (dia == "1")
+                    result.Add(DayOfWeek.Sunday);
+                if (dia == "2")
+                    result.Add(DayOfWeek.Monday);
+                if (dia == "3")
+                    result.Add(DayOfWeek.Tuesday);
+                if (dia == "4")
+                    result.Add(DayOfWeek.Wednesday);
+                if (dia == "5")
+                    result.Add(DayOfWeek.Thursday);
+                if (dia == "6")
+                    result.Add(DayOfWeek.Friday);
+                if (dia == "7")
+                    result.Add(DayOfWeek.Saturday);
+            }
+            return result;
+        }
+        
         public static List<DateTime> GetFechasPermitidas(DateTime sapFechaEntrega, bool esProveedorEspecial)
         {
+            // Limpieza de fecha
+            sapFechaEntrega = sapFechaEntrega.Date;
+         
             var fechasPermitidas = new List<DateTime>();
+
+            // TODO mejorar El proveedor solo puede agendar con un máximo tiempo que comprende la semana en curso más 2 semanas
+            if (sapFechaEntrega > DateTime.Today.AddDays(30))
+            {
+                return fechasPermitidas;
+            }
+
+            // TODO MEJORAR obtener 2 semanas atras el dia en curso.
+            var dia = sapFechaEntrega.AddDays(-30);
+
+            while (dia < sapFechaEntrega.AddDays(30))
+            {
+                var fechaValida = ValidarReglasGenerales(dia);
+
+                if (!Regla2(dia))
+                    fechaValida = false;
+
+                if (!Regla3(dia, sapFechaEntrega))
+                    fechaValida = false;
+
+                if (!Regla4(dia))
+                    fechaValida = false;
+
+                if (!Regla15(dia, esProveedorEspecial))
+                    fechaValida = false;
+
+                if (!Regla20(dia))
+                    fechaValida = false;
+
+                if (fechaValida)
+                    fechasPermitidas.Add(dia);
+
+                dia = dia.AddDays(1);
+
+            }
+
+
+            return fechasPermitidas;
+
+
+            /*var fechasPermitidas = new List<DateTime>();
 
             // Dias de semana permitidos de acuerdo a a las reglas del negocio 
             var diasPermitidos = GetDiasDeSemana();
@@ -93,7 +161,7 @@ namespace Ppgz.CitaWrapper
             }
 
 
-            return fechasPermitidas;
+            return fechasPermitidas;*/
         }
 
         private static bool EsFechaValida(DateTime dateTime, bool esProveedorEspecial)
@@ -159,6 +227,7 @@ namespace Ppgz.CitaWrapper
         /// </summary>
         public static bool ValidarReglasGenerales(DateTime fechaCita)
         {
+            // Limpieza de fecha
             var fecha = fechaCita.Date;
             var manana = DateTime.Today.Date.AddDays(1);
 
@@ -185,11 +254,12 @@ namespace Ppgz.CitaWrapper
         /// </summary>
         public static bool Regla2(DateTime fechaCita)
         {
-            var fecha = fechaCita.Date;
+            // Limpieza de fecha
+            fechaCita = fechaCita.Date;
 
             var manana = DateTime.Today.Date.AddDays(1);
 
-            if (fecha != manana) return true;
+            if (fechaCita != manana) return true;
 
             // TODO pasar la hora limite al la tabla de configuraciones
             return DateTime.Now.Hour <= 17;
@@ -201,6 +271,11 @@ namespace Ppgz.CitaWrapper
         /// </summary>
         public static bool Regla3(DateTime fechaCita, DateTime fechaEntregaOrden)
         {
+            // Limpieza de fecha 
+            fechaCita = fechaCita.Date;
+            fechaEntregaOrden = fechaEntregaOrden.Date;
+
+
             //TODO CALCULAR CUANDO ESTOY EN LA ULTIMA DESMANA DEL AÑO
 
 
@@ -221,8 +296,11 @@ namespace Ppgz.CitaWrapper
         /// <summary>
         /// 4.	El proveedor solo puede agendar con un máximo tiempo que comprende la semana en curso más 2 semanas
         /// </summary>
-        public static bool ValidarRegla4(DateTime fechaCita)
-        {
+        public static bool Regla4(DateTime fechaCita)
+        {            
+            // Limpieza de fecha 
+            fechaCita = fechaCita.Date;
+
             //TODO CALCULAR CUANDO ESTOY EN LA ULTIMA DESMANA DEL AÑO
 
 
@@ -243,12 +321,27 @@ namespace Ppgz.CitaWrapper
         }
 
         /// <summary>
-        /// 5.	El proveedor solo tiene las horas comprendidas de 7:am a 8pm de lunes a viernes para seleccionar el horario de entrega
+        /// 5.	El proveedor solo tiene las horas comprendidas de 7:am a 8pm de lunes a viernes para seleccionar 
+        /// el horario de entrega
         /// </summary>
-        public static bool ValidarRegla5()
+        public static bool Regla5()
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// 15.	Los días miércoles solo pueden hacer cita los proveedores marcados como “proveedores especiales” 
+        /// en el área administrativa, los proveedores que no tengan esa propiedad verán el 
+        /// miércoles sin disponibilidad
+        /// </summary>
+        public static bool Regla15(DateTime fechaCita, bool esProveedorEspecial)
+        {            
+            // Limpieza de fecha 
+            fechaCita = fechaCita.Date;
+
+            return !GetDiasEspeciales().Contains(fechaCita.DayOfWeek) || esProveedorEspecial;
+        }
+
         /// <summary>
         /// 16.	Para facilitar la logística en el CEDIS y para una fácil identificación de la cita en la pantalla administrativa de 
         /// las citas del día, se restringe al proveedor a seleccionar múltiples órdenes de compra destinadas a un mismo almacén. 
@@ -258,6 +351,15 @@ namespace Ppgz.CitaWrapper
         public static bool ValidarRegla16()
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 20.	Los días disponibles para agendar cita son los siguientes: Lunes, Martes, Miércoles, Jueves y Viernes.
+        /// Pudiendo activar el día Sábado según haga falta.
+        /// </summary>
+        public static bool Regla20(DateTime fechaCita)
+        {
+            return GetDiasOperativos().Contains(fechaCita.DayOfWeek);
         }
     }
 }
