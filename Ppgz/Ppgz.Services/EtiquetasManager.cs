2f09467292a960fa894ab48249f59bce94956362
Juan Godoy <g.juanch14@gmail.com>
2017-06-29 09:40:52 -0400

    Actualización del modulo de imprasión de etiquetas

---
+++ Ppgz/Ppgz.Services/EtiquetasManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Ppgz.Repository;
+using SapWrapper;
+
+namespace Ppgz.Services
+{
+    public class EtiquetasManager
+    {
+        private readonly Entities _db = new Entities();
+
+        /// <summary>
+        /// Devuelve un hashtable con lo siguiente:
+        /// 1 return: Contiene un datatable con el resultado del rfc
+        /// 2 "csv" el contenido del archivo si hay ordenes correctas.
+        /// </summary>
+        public Hashtable GetArchivoCsv(int proveedorId, int cuentaId, bool etiquetaNazan, string[] ordenes)
+        {
+            var proveedor = _db.proveedores.FirstOrDefault(p => p.Id == proveedorId && p.CuentaId == cuentaId);
+
+            if (proveedor == null)
+            {
+                throw new BusinessException(CommonMensajesResource.ERROR_Proveedor_Id);
+            }
+
+            var sapEtiquetasManager = new SapEtiquetasManager();
+            var resultado = sapEtiquetasManager.GetContenidoCsv(proveedor.NumeroProveedor, etiquetaNazan, ordenes);
+
+            var contenidoCsv = resultado["csv"];
+
+            var hashTable = new Hashtable
+            {
+                {"return", resultado["return"]},
+                {"csv", contenidoCsv}
+            };
+            
+            return hashTable;
+
+        }
+    }
+}
--- Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ImpresionEtiquetasController.cs
+++ Ppgz/Ppgz.Web/Areas/Mercaderia/Controllers/ImpresionEtiquetasController.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using System.Collections;
 using System.Linq;
-using System.Net.Mime;
+using System.Text;
 using System.Web.Mvc;
 using Ppgz.Repository;
 using Ppgz.Services;
@@ -12,12 +12,16 @@ namespace Ppgz.Web.Areas.Mercaderia.Controllers
     [TerminosCondiciones]
     public class ImpresionEtiquetasController : Controller
     {
-        
+
+        private readonly CommonManager _commonManager = new CommonManager();
+        private readonly ProveedorManager _proveedorManager = new ProveedorManager();
+        private readonly EtiquetasManager _etiquetasManager = new EtiquetasManager();
+        private const string NombreVarSession = "etiqueta_csv";
 
         [Authorize(Roles = "MAESTRO-MERCADERIA,MERCADERIA-IMPRESIONETIQUETAS")]
         public ActionResult Index()
         {
-            var commonManager = new CommonManager();
+            /*var commonManager = new CommonManager();
 
             var cuenta = commonManager.GetCuentaUsuarioAutenticado();
         
@@ -29,27 +33,122 @@ namespace Ppgz.Web.Areas.Mercaderia.Controllers
             var db = new Entities();
             
 
-            ViewBag.Etiquetas = db.etiquetas.Where(e=> proveedoresIds.Contains(e.ProveedorId)).ToList();
+            ViewBag.Etiquetas = db.etiquetas.Where(e=> proveedoresIds.Contains(e.ProveedorId)).ToList();*/
+
+            var cuenta = _commonManager.GetCuentaUsuarioAutenticado();
+
+            ViewBag.proveedores = _proveedorManager.FindByCuentaId(cuenta.Id);
+
+ 
 
             return View();
         }
 
+        [Authorize(Roles = "MAESTRO-MERCADERIA,MERCADERIA-IMPRESIONETIQUETAS")]
+        public ActionResult Generar(int proveedorId)
+        {
+            var cuenta = _commonManager.GetCuentaUsuarioAutenticado();
+            var proveedor = _proveedorManager.Find(proveedorId, cuenta.Id);
+            if (proveedor == null)
+            {
+                TempData["FlashError"] = "Proveedor incorrecto";
+                return RedirectToAction("Index"); 
+            }
+
+            ViewBag.Proveedor = proveedor;
+            return View();
+        }
+
+/*
+        [Authorize(Roles = "MAESTRO-MERCADERIA,MERCADERIA-IMPRESIONETIQUETAS")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Generar(int proveedorId, bool nazan, string [] ordenes)
+        {
+            var cuenta = _commonManager.GetCuentaUsuarioAutenticado();
+
+            var proveedor = _proveedorManager.Find(proveedorId, cuenta.Id);
+
+            if (proveedor == null)
+            {
+                TempData["FlashError"] = "Proveedor incorrecto";
+                return RedirectToAction("Index");
+            }
+
+            var resultado = _etiquetasManager.GetArchivoCsv(proveedorId, cuenta.Id, nazan, ordenes);
+
+            TempData["resultado"] = resultado;
+            TempData["proveedor"] = proveedor;
+
+            return RedirectToAction("Resultado"); 
+        }*/
 
         [Authorize(Roles = "MAESTRO-MERCADERIA,MERCADERIA-IMPRESIONETIQUETAS")]
-        public FileResult Descargar(int id)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Generar(int proveedorId, bool nazan, string orden1, string orden2 = "", string orden3 = "", string orden4 = "", string orden5 = "")
         {
-            var db = new Entities();
-            var etiqueta = db.etiquetas.Find(id);
-            if (etiqueta == null)
-            {
-                // TODO
-                throw new Exception("Etiqueta Incorrecta");
-            }
-             var fileBytes = System.IO.File.ReadAllBytes(etiqueta.Archivo);
-            
-            var fileName = string.Format("Etiqueta_{0}.csv",etiqueta.NumeroOrden );
+            var cuenta = _commonManager.GetCuentaUsuarioAutenticado();
 
-            return File(fileBytes, MediaTypeNames.Application.Pdf, fileName);
+            var proveedor = _proveedorManager.Find(proveedorId, cuenta.Id);
+
+            if (proveedor == null)
+            {
+                TempData["FlashError"] = "Proveedor incorrecto";
+                return RedirectToAction("Index");
+            }
+
+            var ordenes = new[]
+            {
+                orden1, orden2, orden3, orden4, orden5
+            };
+
+            var resultado = _etiquetasManager.GetArchivoCsv(proveedorId, cuenta.Id, nazan, ordenes.Where(o => !string.IsNullOrEmpty(o)).ToArray());
+
+            TempData["resultado"] = resultado;
+            TempData["proveedor"] = proveedor;
+
+            return RedirectToAction("Resultado");
         }
+
+
+        [Authorize(Roles = "MAESTRO-MERCADERIA,MERCADERIA-IMPRESIONETIQUETAS")]
+        public ActionResult Resultado()
+        {
+            if (System.Web.HttpContext.Current.Session[NombreVarSession] != null)
+            {
+                System.Web.HttpContext.Current.Session.Remove(NombreVarSession);
+            }
+
+            var resultado = TempData["resultado"] as Hashtable;
+            var proveedor = TempData["proveedor"] as proveedore;
+
+            if (resultado == null || proveedor == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+
+            ViewBag.Proveedor = proveedor;
+            ViewBag.Resultado = resultado["return"];
+
+            ViewBag.PuedeDescargar = false;
+
+            if (resultado["csv"] == null) return View();
+            if (string.IsNullOrWhiteSpace(resultado["csv"].ToString())) return View();
+
+            System.Web.HttpContext.Current.Session[NombreVarSession] = resultado["csv"];
+            ViewBag.PuedeDescargar = true;
+            return View();
+        }
+
+        [Authorize(Roles = "MAESTRO-MERCADERIA,MERCADERIA-IMPRESIONETIQUETAS")]
+        public FileContentResult Descargar()
+        {
+            var csv = System.Web.HttpContext.Current.Session["etiqueta_csv"].ToString(); 
+
+            return File(new UTF8Encoding().GetBytes(csv), "text/csv", "etiquetas.csv");
+        }
+
     }
 }
