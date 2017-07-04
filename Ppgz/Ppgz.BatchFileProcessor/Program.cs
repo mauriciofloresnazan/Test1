a18a674e348c831ddff516c00932f397140e4fcb
Juan Godoy <g.juanch14@gmail.com>
2017-07-03 21:11:29 -0400

    Ajustes procesador de archivos - Eliminación de funcionalidad de Etiquetas

--- Ppgz/Ppgz.BatchFileProcessor/Program.cs
+++ Ppgz/Ppgz.BatchFileProcessor/Program.cs
@@ -14,25 +14,8 @@ namespace Ppgz.BatchFileProcessor
         private static string _crInboxPath;
         private static string _crPath;
 
-        private static string _etiquetasInboxPath;
-        private static string _etiquetasPath;
-
         private static readonly Entities Db = new Entities();
-        static internal void InitEtiquetas()
-        {
-            var fileSystemWatcher = new FileSystemWatcher
-            {
-                // TODO MOVER AL ARCHIVO DE CONFIGURACION DEL LA 
-                // APLCIACION WEB Y PASARLO COMO ARGUMENTO
-                Path = _etiquetasInboxPath,
-                Filter = "*.*"
-
-            };
-
-            fileSystemWatcher.Changed += etiquetasWatcher_Changed;
-            fileSystemWatcher.EnableRaisingEvents = true;
-        }
-
+        
         static void Main()
         {
             _wrongFilesPath = Db.configuraciones.Single(c => c.Clave == "batchfile.wrongfilespath").Valor;
@@ -40,9 +23,7 @@ namespace Ppgz.BatchFileProcessor
             _crInboxPath = Db.configuraciones.Single(c => c.Clave == "batchfile.crinboxpath").Valor;
             _crPath = Db.configuraciones.Single(c => c.Clave == "batchfile.crpath").Valor;
 
-            _etiquetasInboxPath = Db.configuraciones.Single(c => c.Clave == "batchfile.etiquetasinboxpath").Valor;
-            _etiquetasPath = Db.configuraciones.Single(c => c.Clave == "batchfile.etiquetaspath").Valor;
-
+        
             var crWatcher = new FileSystemWatcher
             {
                 // TODO MOVER AL ARCHIVO DE CONFIGURACION DEL LA 
@@ -54,8 +35,7 @@ namespace Ppgz.BatchFileProcessor
             crWatcher.Changed += crWatcher_Changed;
             crWatcher.EnableRaisingEvents = true;
 
-            InitEtiquetas();
-
+            
             Console.ReadLine();
         }
 
@@ -88,38 +68,7 @@ namespace Ppgz.BatchFileProcessor
             }
         }
 
-        static void etiquetasWatcher_Changed(object sender, FileSystemEventArgs e)
-        {
-            try
-            {
-                if (Path.GetExtension(e.FullPath) != ".csv")
-                {
-                    throw new Exception("Archivo incorrecto");
-                }
-
-                var archivoSinExtension = Path.GetFileNameWithoutExtension(e.FullPath);
-
-                if (archivoSinExtension == null)
-                {
-                    return;
-                }
-
-                var numeroProveedor = archivoSinExtension.Split('_')[1];
-                var numeroOrden = archivoSinExtension.Split('_')[2];
-
-                var fecha = DateTime.Now;
-
-                MoverEtiqueta(e.FullPath, fecha, numeroOrden, numeroProveedor);
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.Message);
-                MoverError(e.FullPath);
-
-            }
-
-        }
-
+        
         static void MoverError(string path)
         {
             try
@@ -202,72 +151,6 @@ namespace Ppgz.BatchFileProcessor
                 cr.Fecha = fecha;
                 cr.ArchivoCR = newPath;
                 Db.Entry(cr).State = EntityState.Modified;
-            }
-
-            Db.SaveChanges();
-
-
-        }
-
-        static void MoverEtiqueta(string path, DateTime fecha, string numeroOrden, string numeroProveedor)
-        {
-
-            if (!File.Exists(path)) return;
-
-            var fileName = Path.GetFileName(path);
-
-            if (fileName == null) return;
-
-            var proveedor = Db.proveedores.FirstOrDefault(p => p.NumeroProveedor == numeroProveedor);
-            if (proveedor == null)
-            {
-                throw new Exception("Proveedor incorrecto");
-            }
-
-            var etiquetaRottPath = new DirectoryInfo(_etiquetasPath);
-
-            var yearPath = etiquetaRottPath.GetDirectories(fecha.Year.ToString()).Length == 0 ?
-                etiquetaRottPath.CreateSubdirectory(fecha.Year.ToString()) :
-                etiquetaRottPath.GetDirectories(fecha.Year.ToString())[0];
-
-            var monthPath = yearPath.GetDirectories(fecha.Month.ToString()).Length == 0 ?
-                yearPath.CreateSubdirectory(fecha.Month.ToString()) :
-                yearPath.GetDirectories(fecha.Month.ToString())[0];
-
-            var newPath = Path.Combine
-                (monthPath.FullName, fileName);
-
-            if (File.Exists(newPath))
-            {
-                File.Delete(newPath);
-                Thread.Sleep(100);
-            }
-
-            WaitForFile(path);
-
-            File.Move(path, newPath);
-
-
-            var proveedorId = proveedor.Id;
-
-            var etiqueta = Db.etiquetas.FirstOrDefault(et => et.NumeroOrden == numeroOrden
-                && et.ProveedorId == proveedorId && et.Fecha == fecha.Date);
-            if (etiqueta == null)
-            {
-                etiqueta = new etiqueta
-                {
-                    Fecha = fecha,
-                    NumeroOrden = numeroOrden,
-                    ProveedorId = proveedorId,
-                    Archivo = newPath
-                };
-                Db.Entry(etiqueta).State = EntityState.Added;
-            }
-            else
-            {
-                etiqueta.Archivo = newPath;
-
-                Db.Entry(etiqueta).State = EntityState.Modified;
             }
 
             Db.SaveChanges();
