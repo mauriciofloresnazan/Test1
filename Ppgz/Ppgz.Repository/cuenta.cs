f950b87173a82f700a56ec75d95df8403a9563b1
Rainer Ramirez <ironrai@gmail.com>
2018-10-09 16:11:00 -0500

    Update Cuenta cs

--- Ppgz/Ppgz.Repository/cuenta.cs
+++ Ppgz/Ppgz.Repository/cuenta.cs
@@ -30,6 +30,7 @@ namespace Ppgz.Repository
         public string Tipo { get; set; }
         public bool EsEspecial { get; set; }
         public bool SinASN { get; set; }
+        public bool Factoraje { get; set; }
         public bool Activo { get; set; }
         public bool Borrado { get; set; }
         public Nullable<System.DateTime> FechaTx { get; set; }
@@ -43,46 +44,4 @@ namespace Ppgz.Repository
         public virtual ICollection<proveedore> proveedores { get; set; }
         public virtual ICollection<AspNetUser> AspNetUsers { get; set; }
     }
-
-
-    public int Id { get; set; }
-
-    public string CodigoProveedor { get; set; }
-
-    public string NombreCuenta { get; set; }
-
-    public Nullable<System.DateTime> FechaRegistro { get; set; }
-
-    public string Tipo { get; set; }
-
-    public bool EsEspecial { get; set; }
-
-    public bool SinASN { get; set; }
-
-    public bool Factoraje { get; set; }
-
-    public bool Activo { get; set; }
-
-    public bool Borrado { get; set; }
-
-    public Nullable<System.DateTime> FechaTx { get; set; }
-
-    public string UsuarioIdTx { get; set; }
-
-    public string OperacionTx { get; set; }
-
-
-
-    public virtual AspNetUser AspNetUser { get; set; }
-
-    public virtual ICollection<cuentaproveedore> cuentaproveedores { get; set; }
-
-    public virtual ICollection<perfile> perfiles { get; set; }
-
-    public virtual ICollection<cuentasmensaje> cuentasmensajes { get; set; }
-
-    public virtual ICollection<proveedore> proveedores { get; set; }
-
-    public virtual ICollection<AspNetUser> AspNetUsers { get; set; }
-
 }
